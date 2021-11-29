be68b109797ae6105440262523ba4ff0e298a12f
Sean Chen <qianlonwork@outlook.com>
2021-11-29 13:03:06 +0800

    fix: 🐛 修复 api 请求路径

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,10 +2,10 @@ export const UPLOAD_API = "http://175.24.185.110:5200";
 // export const UPLOAD_API = "http://localhost:5200";
 //
 
-export const BASE_API = "http://localhost:3100";
-export const SOCKETIO_API = "http://localhost:3100";
-export const WS_API = "ws://localhost:7000";
-// export const BASE_API = "http://175.24.185.110:3100";
-// export const SOCKETIO_API = "http://175.24.185.110:3100";
-// export const WS_API = "ws://175.24.185.110:7000";
+// export const BASE_API = "http://localhost:3100";
+// export const SOCKETIO_API = "http://localhost:3100";
+// export const WS_API = "ws://localhost:7000";
+export const BASE_API = "http://175.24.185.110:3100";
+export const SOCKETIO_API = "http://175.24.185.110:3100";
+export const WS_API = "ws://175.24.185.110:7000";
 //
