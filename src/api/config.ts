4b0ab7c2a3bc847b1f76df74eea89f18bef9de01
Sean Chen <qianlonwork@outlook.com>
2022-03-08 22:27:11 +0800

    feat: 🎸 添加 env 文件

--- src/api/config.ts
+++ src/api/config.ts
@@ -5,7 +5,10 @@ export const UPLOAD_API = "http://175.24.185.110:5200";
 // export const BASE_API = "http://localhost:3100";
 // export const SOCKETIO_API = "http://localhost:3100";
 // export const WS_API = "ws://localhost:7000";
-export const BASE_API = "http://175.24.185.110:3100";
-export const SOCKETIO_API = "http://175.24.185.110:3100";
-export const WS_API = "ws://175.24.185.110:7000";
+
+//"http://175.24.185.110:3100"
+// process.env.VUE_APP_BASE_URL
+export const BASE_API = process.env.VUE_APP_BASE_URL as string;
+export const SOCKETIO_API = process.env.VUE_APP_BASE_URL as string;
+export const WS_API = process.env.VUE_APP_BASE_URL as string;
 //
